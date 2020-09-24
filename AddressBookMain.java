406f003c4107daedcadb61e7149ce08368e34995
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-24 19:51:06 +0530

    class Contacts created

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,3 +1,92 @@
+class Contacts{
+    private String fName;
+    private String lName;
+    private String address;
+    private String city;
+    private String state;
+    private double zip;
+    private String phone;
+    private String email;
+
+    public Contacts(String fName, String lName, String address, String city, String state, double zip, String phone,
+            String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getZip() {
+        return zip;
+    }
+
+    public void setZip(double zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+}
+
 public class AddressBookMain{
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
