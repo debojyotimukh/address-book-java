95ccfb4ca0ed92895a4777be28f7a83ddd18a88e
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-30 17:23:22 +0530

    Contacts modified to Contact; duplicate checking added in addContact()

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,89 +1,5 @@
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Scanner;
-
-class Contact {
-    private String fName;
-    private String lName;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private String phone;
-    private String email;
-
-    public Contact(String fName, String lName, String address, String city, String state, String zip, String phone,
-                   String email) {
-        this.fName = fName;
-        this.lName = lName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return fName + "\t" + lName + "\t" + address + "\t" + city + "\t" + state + "\t" + zip + "\t" + phone + "\t"
-                + email;
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-}
-
-class AddressBookService {
-    private ArrayList<Contact> contactList = new ArrayList<>();
-
-    public void addContact(Contact contact) {
-        contactList.add(contact);
-    }
-
-    public int searchByName(String name) {
-        for (Contact contact : contactList)
-            if (contact.getfName().equalsIgnoreCase(name))
-                return contactList.indexOf(contact);
-        return -1;
-    }
-
-    public boolean editContact(String name, Contact modified) {
-        int index = searchByName(name);
-        if (index == -1)
-            return false;
-        contactList.set(index, modified);
-        return true;
-    }
-
-    public boolean deleteContact(String name) {
-        int index = searchByName(name);
-        if (index == -1)
-            return false;
-        contactList.remove(index);
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        if (contactList.isEmpty())
-            return "No contacts found!";
-
-        StringBuilder sBuilder = new StringBuilder();
-        for (Contact contacts : contactList)
-            sBuilder.append(contacts.toString()).append("\n");
-
-        return sBuilder.toString();
-    }
-}
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AddressBookMain {
     private static Contact readContact(Scanner sc) {
@@ -120,7 +36,9 @@ public class AddressBookMain {
             sc.nextLine();
             switch (choice) {
                 case 1:
-                    book.addContact(readContact(sc));
+                    if (book.addContact(readContact(sc)))
+                        System.out.println("Contact Added!");
+                    else System.out.println("Contact already exists");
                     break;
                 case 2:
                     System.out.println("Enter name to edit: ");
@@ -154,7 +72,7 @@ public class AddressBookMain {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
         Dictionary<String, AddressBookService> library = new Hashtable<>();
         while (true) {
             System.out.println("\n\nWelcome to Address Book Program");
@@ -175,12 +93,12 @@ public class AddressBookMain {
                     break;
                 case 2:
                     System.out.println("Availble books are: ");
-                    for (Enumeration<String> i = library.keys(); i.hasMoreElements();) {
+                    for (Enumeration<String> i = library.keys(); i.hasMoreElements(); ) {
                         System.out.println(i.nextElement() + ",");
                     }
                     System.out.println("Open Book: ");
                     String name = sc.nextLine();
-                    System.out.println("Current: "+name);
+                    System.out.println("Current: " + name);
                     addressBookOps(library.get(name), sc);
                     break;
                 case 3:
