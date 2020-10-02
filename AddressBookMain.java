deb34a6aa3aa34e2560f508c45984e83c511135b
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-02 23:02:16 +0530

    refactor, search by name, city, state implement

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,48 +1,95 @@
 import java.util.*;
 
 public class AddressBookMain {
-    private static int welcomePrompt(Scanner sc) {
+    //private static final Dictionary<String, AddressBookService> library = new Hashtable<>();
+    private static final List<AddressBookService> library=new ArrayList<>();
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static int locateIndex(String name){
+        for(int i=0;i<library.size();i++)
+            if(library.get(i).getBookName().equals(name))
+                return i;
+        return -1;
+    }
+
+    private static int welcomePrompt() {
         System.out.println("\n\nWelcome to Address Book Program");
         System.out.println("1. New Address Book");
         System.out.println("2. Select Book");
         System.out.println("3. Delete Book");
-        System.out.println("4. Quit");
+        System.out.println("4. Search");
+        System.out.println("5. Quit");
         System.out.print("Your choice: ");
-        int choice = sc.nextInt();
-        sc.nextLine();
+        int choice = AddressBookMain.sc.nextInt();
+        AddressBookMain.sc.nextLine();
         return choice;
     }
 
+    private static void searchByPrompt() {
+        System.out.println("1. By name");
+        System.out.println("2. By city");
+        System.out.println("3. By state");
+        System.out.println("4. Back");
+        System.out.println("Your choice: ");
+        int choice =sc.nextInt();
+        sc.nextLine();
+        switch (choice){
+            case 1:
+                System.out.println("Enter name: ");
+                String name=sc.nextLine();
+                library.forEach(book->book.searchByName(name).forEach(System.out::println));
+                break;
+            case 2:
+                System.out.println("Enter city: ");
+                String city=sc.nextLine();
+                library.forEach(book->book.searchByCity(city).forEach(System.out::println));
+                break;
+            case 3:
+                System.out.println("Enter state: ");
+                String state=sc.nextLine();
+                library.forEach(book->book.searchByState(state).forEach(System.out::println));
+                break;
+            case 4:
+                return;
+            default:
+                System.out.println("INVALID CHOICE!");
+        }
+
+    }
+
+    private static void addBook(String bookName) {
+        AddressBookService addressBookService=new AddressBookService();
+        addressBookService.setBookName(bookName);
+        library.add(addressBookService);
+        AddressBookCLI.run(addressBookService, sc);
+    }
+
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        Dictionary<String, AddressBookService> library = new Hashtable<>();
         while (true) {
-            switch (welcomePrompt(sc)) {
-                case 1:
+            switch (welcomePrompt()) {
+                case 1: //add
                     System.out.println("Name of new address book: ");
                     String bookName = sc.next();
                     sc.nextLine();
-                    library.put(bookName, new AddressBookService());
-                    AddressBookCLI.run(library.get(bookName), sc);
+                    addBook(bookName);
                     break;
-                case 2:
+                case 2: //select
                     System.out.println("Available books are: ");
-                    for (Enumeration<String> i = library.keys(); i.hasMoreElements(); ) {
-                        System.out.println(i.nextElement() + ",");
-                    }
+                    library.forEach(book->System.out.println(book.getBookName()+" ,"));
                     System.out.println("Open Book: ");
                     String name = sc.nextLine();
                     System.out.println("Current: " + name);
-                    AddressBookCLI.run(library.get(name), sc);
+                    AddressBookCLI.run(library.get(locateIndex(name)), sc);
                     break;
-                case 3:
+                case 3: //delete
                     System.out.println("Enter name to delete: ");
                     name = sc.nextLine();
-                    library.remove(name);
+                    library.remove(locateIndex(name));
                     break;
                 case 4:
+                    searchByPrompt();
+                    break;
+                case 5: //quit
                     sc.close();
                     return;
                 default:
@@ -51,6 +98,7 @@ public class AddressBookMain {
             }
         }
     }
+
 
 
 }
--- AddressBookService.java
+++ AddressBookService.java
@@ -4,6 +4,15 @@ import java.util.stream.Collectors;
 
 public class AddressBookService {
     private final ArrayList<Contact> contactList = new ArrayList<>();
+    private String bookName;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
 
     public boolean addContact(Contact contact) {
         List<Contact> filteredByFName = searchByName(contact.getfName());
@@ -14,14 +23,26 @@ public class AddressBookService {
         return true;
     }
 
-    public List<Contact> searchByName(String name){
+    public List<Contact> searchByName(String name) {
         return contactList.stream().
                 filter(person -> person.getfName().equalsIgnoreCase(name)).
                 collect(Collectors.toList());
     }
 
+    public List<Contact> searchByCity(String city) {
+        return contactList.stream().
+                filter(person -> person.getCity().equalsIgnoreCase(city)).
+                collect(Collectors.toList());
+    }
+
+    public List<Contact> searchByState(String state) {
+        return contactList.stream().
+                filter(person -> person.getState().equalsIgnoreCase(state)).
+                collect(Collectors.toList());
+    }
+
     public boolean editContact(Contact current, Contact modified) {
-        if(!contactList.contains(current))
+        if (!contactList.contains(current))
             return false;
         contactList.remove(current);
         contactList.add(modified);
