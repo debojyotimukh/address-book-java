22301dc27f3ef5609e45d3f6fa1fb5a5ee1cc832
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-15 01:17:50 +0530

    refactor: create contact read and write

--- addressbook/src/main/java/com/capgemini/training/java/CSVUtils.java
+++ addressbook/src/main/java/com/capgemini/training/java/CSVUtils.java
@@ -5,7 +5,6 @@ import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.opencsv.CSVWriter;
@@ -13,31 +12,34 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 public class CSVUtils {
     public static List<Contact> loadContactsFromCSV(final String filepath) throws IOException {
-        Reader reader = Files.newBufferedReader(Paths.get(filepath));
+        List<Contact> contactList = null;
 
-        CsvToBean<Object> csvToBean = new CsvToBeanBuilder<>(reader).withType(Contact.class)
-                .withIgnoreLeadingWhiteSpace(true).build();
-        List<Object> contactList = csvToBean.parse();
-        List<Contact> result = new ArrayList<>();
-        for (Object contactBean : contactList) {
-            Contact contact = (Contact) contactBean;
-            result.add(contact);
+        try (Reader reader = Files.newBufferedReader(Paths.get(filepath));) {
+
+            CsvToBean<Contact> csvToBean = new CsvToBeanBuilder<Contact>(reader).withType(Contact.class).build();
+            contactList = csvToBean.parse();
+
+        } catch (Exception e) {
+            // TODO: handle exception
         }
-        return result;
+
+        return contactList;
     }
 
-    public static void writeContactsInCSV(final List<Contact> contacts,final String filepath)
-            throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+    public static void writeContactsInCSV(List<Contact> contacts, final String filepath) {
 
-        Writer writer = Files.newBufferedWriter(Paths.get(filepath));
-        StatefulBeanToCsv<Contact> beanToCsv = new StatefulBeanToCsvBuilder<Contact>(writer)
-                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER).build();
-        
-        beanToCsv.write(contacts);
+        try (Writer writer = Files.newBufferedWriter(Paths.get(filepath));) {
+
+            StatefulBeanToCsv<Contact> beanToCsv = new StatefulBeanToCsvBuilder<Contact>(writer)
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER).build();
+            beanToCsv.write(contacts);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
